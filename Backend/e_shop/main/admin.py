1f147972452b4d6d4627c128b7cb4b2ae7361b11
VariA sMOL <96552183+VariaSmol@users.noreply.github.com>
2024-07-22 23:34:47 -0400

    Create models

--- Backend/e_shop/main/admin.py
+++ Backend/e_shop/main/admin.py
@@ -7,4 +7,8 @@ admin.site.register(Brand)
 admin.site.register(Color)
 admin.site.register(Size) 
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display=('id', 'title', 'brand', 'color', 'size', 'rate', 'status')
+    list_editable=('status',)
+admin.site.register(Product)
 
--- Backend/e_shop/main/models.py
+++ Backend/e_shop/main/models.py
@@ -27,3 +27,19 @@ class Size(models.Model):
 
     def __str__(self): 
         return self.title
+
+class Product(models.Model): 
+    title=models.CharField(max_length=100)
+    image=models.ImageField(upload_to="product_imgs/")
+    slug= models.CharField(max_length=400) 
+    detail=models.TextField()
+    specs=models.TextField()
+    price=models.PositiveIntegerField()
+    category=models.ForeignKey(Category, on_delete=models.CASCADE)
+    brand=models.ForeignKey(Brand, on_delete=models.CASCADE)
+    color=models.ForeignKey(Color, on_delete=models.CASCADE)
+    size=models.ForeignKey(Size, on_delete=models.CASCADE)
+    status=models.BooleanField(default=True)
+
+    def __str__(self): 
+        return self.title
