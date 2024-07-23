6038c9797c49a501afef1f0a06e9ee47bc2bf04c
VariA sMOL <96552183+VariaSmol@users.noreply.github.com>
2024-07-22 23:01:45 -0400

    Add models

--- Backend/e_shop/main/models.py
+++ Backend/e_shop/main/models.py
@@ -1,3 +1,29 @@
+
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title=models.CharField(max_length=100)
+    image=models.ImageField(upload_to="cat_imgs/")
+    
+    def __str__(self):
+        return self.title
+
+class Brand(models.Model): 
+    title=models.CharField(max_length=100)
+    image=models.ImageField(upload_to="brand_imgs/")
+
+    def __str__(self): 
+        return self.title
+
+class Color(models.Model):
+    title=models.CharField(max_length=100)
+    color_code=models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.title
+
+class Size(models.Model): 
+    title=models.CharField(max_length=100)
+
+    def __str__(self): 
+        return self.title
